2f38a18870e166b05ba8a0953fcd3beb2f9c47e2
Abdul Halim <abxlim@users.noreply.github.com>
2025-05-23 20:33:03 +0700

    fix: Correct import path and update test description
    - Memperbaiki jalur impor dari "indexs.js" menjadi "index.js"
    - Mengubah deskripsi pengujian untuk mencerminkan fungsi yang diuji

--- 03-writing-test/index.test.js
+++ 03-writing-test/index.test.js
@@ -1,9 +1,9 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { sum } from "./indexs.js";
+import { sum } from "./index.js";
 
 describe("sum function", () => {
-  it("should return sum of two numbers", () => {
+  it("mengembalikan penjumlahan", () => {
     const actual = sum(1, 2);
     const expected = 3;
     assert.equal(actual, expected);
